f31e0152d4314cd58471cd8b62da583f47e72dd9
Pozzame <pozzame@gmail.com>
2024-06-25 14:49:38 +0200

    Commenti

--- Program.cs
+++ Program.cs
@@ -1,21 +1,26 @@
 ﻿Random rng = new Random();
 int segreto = rng.Next(100);
 Console.WriteLine("Quanti tentativi vuoi?");
-int prove = Convert.ToInt32(Console.ReadLine());
+int tentativi = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Prova un numero. (1-100)");
 
-while (prove > 0)
+while (tentativi > 0) //prova finché non ha finito i tentativi
 {
     int num = Convert.ToInt32(Console.ReadLine());
-    if (num == segreto)
+    if (num > 100)
+    {
+        Console.WriteLine("Sei ansato fuori range.");
+        tentativi = -1; //Esce senza dire che hai perso
+    }
+    else if (num == segreto)
     {
         Console.WriteLine("Bravo! :-D");
-        prove = -1;
+        tentativi = -1; //Esce senza dire che hai perso
     }
     else if (num < segreto)
         Console.WriteLine("Prova più alto.");
     else if (num > segreto)
         Console.WriteLine("Prova più basso.");
-    prove--;
+    tentativi--;
 }
-if (prove >= 0 ) Console.WriteLine("Spiacente, hai perso. :-(");
+if (tentativi == 0 ) Console.WriteLine("Spiacente, hai perso. :-(");
