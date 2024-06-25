f2bbed9b425d90b606c8b7de6e97869964fa4f7c
Pozzame <pozzame@gmail.com>
2024-06-25 11:55:38 +0200

    Prima

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Indovina il numero");
+
+Random rng = new Random();
+
+int indice = 10;
+while (Convert.ToBoolean(indice))
+{
+    Console.WriteLine(rng.Next(1000));
+    indice --;
+}
