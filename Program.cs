f759911d74ffd8337ca24e3be51e870d6c2d7157
Pozzame <pozzame@gmail.com>
2024-06-25 11:56:12 +0200

    Prima

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿Console.WriteLine("Indovina il numero");
+﻿Console.WriteLine("Quante iterazioni?");
+int iterazioni = Convert.ToInt32(Console.ReadLine());
 
 Random rng = new Random();
+int prove = 0;
 
-int indice = 10;
-while (Convert.ToBoolean(indice))
+while (Convert.ToBoolean(iterazioni))
 {
-    Console.WriteLine(rng.Next(1000));
-    indice --;
+    int num = rng.Next(1000);
+
+    while(Trovatore(num) == num);
+    
+    iterazioni--;
+}
+
+int Trovatore(int num)
+{
+    while (num == rng.Next(1000))
+        {
+            return prova;
+            prove++;
+        }
 }
